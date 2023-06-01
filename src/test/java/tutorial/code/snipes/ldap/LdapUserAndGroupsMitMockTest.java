f1bf3796fc1b70c22a6729804d565c111d4fa29c
Iam4Testinghere <patrick.wiget@gmx.ch>
2023-06-01 11:19:30 +0200

    Zusammenfassung der hinzugefügten Funktionen:
    - Hinzugefügt: Neue Methode "getAllAttributeOfName" in der Klasse "LdapUserAndGroups", um alle Attribute eines bestimmten Namens aus dem LDAP-Verzeichnis abzurufen.
    - Hinzugefügt: Unit-Test für die Methode "getAllAttributeOfName" in der Klasse "LdapUserAndGroupsTest".

--- src/test/java/tutorial/code/snipes/ldap/LdapUserAndGroupsMitMockTest.java
+++ src/test/java/tutorial/code/snipes/ldap/LdapUserAndGroupsMitMockTest.java
@@ -2,13 +2,22 @@ package tutorial.code.snipes.ldap;
 
 import org.junit.Rule;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.zapodot.junit.ldap.EmbeddedLdapRule;
 import org.zapodot.junit.ldap.EmbeddedLdapRuleBuilder;
 
+import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
-import javax.naming.directory.DirContext;
+import javax.naming.directory.*;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 class LdapUserAndGroupsMitMockTest {
     /**
      * Diese Klasse enthält JUnit-Tests für die LdapUserAndGroups-Klasse.
@@ -52,13 +61,43 @@ class LdapUserAndGroupsMitMockTest {
         assertNotNull(connection);
     }
     /**
-     * Eine weitere Testmethode, die noch nicht implementiert ist.
-     * @throws Exception - Wenn ein Fehler auftritt.
+     * Diese Klasse enthält Unit-Tests für die Methode "getAllAttributeOfName" der Klasse "LdapUserAndGroups".
+     * Sie überprüft, ob die Methode alle Attribute eines bestimmten Namens aus dem LDAP-Verzeichnis abruft.
      */
     @Test
-    public void testwas() throws Exception {
-        LdapUserAndGroups ldapUserAndGroups = new LdapUserAndGroups();
-        //  ldapUserAndGroups.testwas();
+    public void shouldReturnAllAttributesForGivenName() throws NamingException {
+        // Arrange
+        DirContext mockDirContext = mock(DirContext.class);
+        NamingEnumeration<SearchResult> mockNamingEnumeration = mock(NamingEnumeration.class);
+        SearchResult mockSearchResult = mock(SearchResult.class);
+        Attributes mockAttributes = mock(Attributes.class);
+        NamingEnumeration  mockAttributeEnumeration = mock(NamingEnumeration.class);
+        Attribute mockAttribute = mock(Attribute.class);
+        NamingEnumeration mockValueEnumeration = mock(NamingEnumeration.class);
+
+        LdapUserAndGroups ldapUserAndGroups = mock(LdapUserAndGroups.class, Mockito.CALLS_REAL_METHODS);
+        ldapUserAndGroups.connection = mockDirContext;
+
+        // Mock behaviour
+        Mockito.doReturn(mockDirContext).when(ldapUserAndGroups).getnewConnection();
+        when(mockDirContext.search(anyString(), anyString(), any(SearchControls.class))).thenReturn(mockNamingEnumeration);
+        when(mockNamingEnumeration.hasMore()).thenReturn(true, false);
+        when(mockNamingEnumeration.nextElement()).thenReturn(mockSearchResult);
+        when(mockSearchResult.getAttributes()).thenReturn(mockAttributes);
+        when(mockAttributes.getAll()).thenReturn(mockAttributeEnumeration);
+        when(mockAttributeEnumeration.hasMore()).thenReturn(true, false);
+        when(mockAttributeEnumeration.next()).thenReturn(mockAttribute);
+        when(mockAttribute.getID()).thenReturn("cn");
+        when(mockAttribute.getAll()).thenReturn(mockValueEnumeration);
+        when(mockValueEnumeration.hasMore()).thenReturn(true, false);
+        when(mockValueEnumeration.next()).thenReturn("testuser");
+
+        // Act
+        List<String> allAttributes = ldapUserAndGroups.getAllAttributeOfName("testuser");
+
+        // Assert
+        assertEquals(1, allAttributes.size());
+        assertEquals("cn: testuser", allAttributes.get(0));
     }
 }
 
